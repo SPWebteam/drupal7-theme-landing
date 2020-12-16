1e0a62c224c1a9a8b997194b026a002a47ee463d
chriseikelboom <ceikelboom@sp.nl>
2020-12-16 11:43:43 +0100

    added counter

--- js/countdown.js
+++ js/countdown.js
@@ -6,8 +6,8 @@
       var countdown_id = "countdown";
 
       // set the date we're counting down to
-      var target_date = new Date("03/21/2018 07:30:00").getTime();  //ONLY USE THIS FORMAT FOR SAFARI SUPPORT! mm/dd/yyyy hh:mm:ss
-      var final_date = new Date("03/21/2018 21:00:00").getTime();
+      var target_date = new Date("03/17/2021 07:30:00").getTime();  //ONLY USE THIS FORMAT FOR SAFARI SUPPORT! mm/dd/yyyy hh:mm:ss
+      var final_date = new Date("03/17/2021 21:00:00").getTime();
 
       // variables for time units
       var days, hours, minutes, seconds;
@@ -80,7 +80,7 @@
 
           // format countdown string + set tag value
           if (countdown_status == "first") {
-            //countdown.innerHTML = "Stem 21 Maart<br>Gemeenteraadsverkiezingen<br>" + days_string + hours + ":" + minutes + ":" + seconds + " uur";
+            countdown.innerHTML = "17 Maart<br>Tweede Kamerverkiezingen<br>" + days_string + hours + ":" + minutes + ":" + seconds + " uur";
             days_span.innerHTML = days;
             hours_span.innerHTML = hours;
             minutes_span.innerHTML = minutes;
@@ -88,14 +88,14 @@
 
           }
           if (countdown_status == "second") {
-            //countdown.innerHTML = "Stem SP!<br>" + hours + ":" + minutes + ":" + seconds + " uur";
+            countdown.innerHTML = "Stem SP!<br>" + hours + ":" + minutes + ":" + seconds + " uur";
             days_span.innerHTML = days;
             hours_span.innerHTML = hours;
             minutes_span.innerHTML = minutes;
             seconds_span.innerHTML = seconds;
           }
           if (countdown_status == "final") {
-            //countdown.innerHTML = "SP stemmers bedankt!";
+            countdown.innerHTML = "SP stemmers bedankt!";
             days_span.innerHTML = "0"; // else NaN error
             hours_span.innerHTML = "0"
             minutes_span.innerHTML = "0"
